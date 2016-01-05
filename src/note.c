2a354e16e37093e9bc299731462184e4da2d37b2
Sam Pagenkopf <ssaammp@gmail.com>
2016-01-05 15:28:22 -0600

    made note.c more specific

--- src/note.c
+++ src/note.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
-#define MIDDLE_C 440.0
-#define STARTING_NOTE 100
-#define ROOT_12_2 1.059463094359 // twelfth root of 2
+// Returns the frequency of a note. Uses MIDI.
 
-// Returns the frequency of a note
-double noteCalc(char noteNum) {
+#define MIDDLE_C 440.0
+#define STARTING_NOTE 60
+#define ROOT_12_2 1.05946309435929526456 // twelfth root of 2
+
+double noteCalcChar(char noteNum) {
 	int relativeNote = noteNum - STARTING_NOTE;
+	return MIDDLE_C * pow( ROOT_12_2, (double)relativeNote );
+}
+
+double noteCalc(double noteNum) {
+	double relativeNote = noteNum - STARTING_NOTE;
 	return MIDDLE_C * pow( ROOT_12_2, (double)relativeNote );
 }
