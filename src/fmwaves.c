aac01228786bbcc1b6d689228571ae1f987f3897
Sam Pagenkopf <ssaammp@gmail.com>
2016-01-05 01:35:31 -0600

    Removed sketchy PI_2 define

--- src/fmwaves.c
+++ src/fmwaves.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
+#define PI_2 2.0 * M_PI
+
 // TODO: make it easier to adjust functions' ranges
 //	e.g. is it useful to have a function with less range?
 
@@ -16,7 +18,7 @@ double absSineFM(double phase) {
 
 // Square wave. Aliased. Used as a carrier for C64 tones.
 double squareFM(double phase) {
-	if( phase % PI_2 >= M_PI ) {
+	if( fmod(phase, PI_2) >= M_PI ) {
 		return( 0 );
 	} else {
 		return( M_PI * 2.0 );
@@ -25,7 +27,7 @@ double squareFM(double phase) {
 
 // Triangle wave. Aliased.
 double triangleFM(double phase) {
-	if( phase % PI_2 >= M_PI ) {
+	if( fmod(phase, PI_2) >= M_PI ) {
 		return(phase * 2.0);
 	} else {
 		return(-2.0 * phase + M_PI);
