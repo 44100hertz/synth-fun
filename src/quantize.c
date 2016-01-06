81a85c3bd84fe1f6c6cbe7dbb187a2478eecc0b4
Sam Pagenkopf <ssaammp@gmail.com>
2016-01-06 08:12:05 -0600

    added hard clipping to quantize function

--- src/quantize.c
+++ src/quantize.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 
+// Turns an FM wave (normally range 0 - 2pi) into a char
+// Out-of-range values are clipped
 char quantizeFMtoChar(double wave) {
-	return (char)(wave / 2.0 / M_PI * 0xFF - 0x7F);
+	output = (wave / 2.0 / M_PI * 0xFF - 0x7F);
+	return (char)fmax( fmin( output, 1.0 ), -1.0 );
 }
