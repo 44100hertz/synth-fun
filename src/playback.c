7f2b25f00a6110e49bf704e8956751e5e7550325
Sam Pagenkopf <ssaammp@gmail.com>
2016-01-07 16:24:55 -0600

    removed stupid byteshift

--- src/playback.c
+++ src/playback.c
@@ -6,13 +6,7 @@
 #include "note.c"
 
 int song(int sampleRate, songData song) {
-struct songData testSong;
 
-testSong.BPM = 120;
-testSong.tickRate = 48;
-testSong.key = {c, d, e, f, g, a, b};
-
-	
 	double phase = 0;
 	int tick = -1;
 	int offset = 0;
@@ -20,15 +14,18 @@ testSong.key = {c, d, e, f, g, a, b};
 	
 	// timer cycle
 	for(long timer = 0; song.pattern != -1; timer++) {
-		if(tick < (tick=timer / sampleRate / 60 * song.BPM * song.tickRate)) {
+		int nextTick = timer / sampleRate / 60 * song.BPM * song.tickRate;
+		
+		if(nextTick > tick) {
 			slope[i] = ( noteCalcChar(song.key[i + offset]) / sampleRate );
 			++offset;
 		}
+		
 		phase = fmod((timer * slope), 1.0) * 2 - 1;
 
 		byteOut = quantize( sineWave(phase) );
 		
-		putchar(byteOut << 8);
+		putchar(byteOut);
 		putchar(byteOut >> 8);
 	}
 }
