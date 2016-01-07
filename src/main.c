0e41e825b0720860e79bcd946d0081b262fba183
Sam Pagenkopf <ssaammp@gmail.com>
2016-01-07 16:16:56 -0600

    removed stupid argv parsing from main

--- src/main.c
+++ src/main.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include "song.c"
-
-enum { 8_BIT, 16_BIT };
+#include "playback.c"
 
 int main(int argc, char *argv[]) {
 
 	int freq = 48000;
-	int depth = 2;
+
+	songData testSong = makesong();
 	
-	for(int i=1; i<(argc-1); i++) {
-		if (strncmp(argv[i], "-r", 2)) {
-			freq = atoi(argv[i+1]);
-			i++;
-		} else if(strncmp(argv[i], "-d", 2)) {
-			int depthBits = atoi(argv[i+1]);
-		}
-	}
-	
-	return song(freq, depth);
+	return song(freq, testSong);
 }
