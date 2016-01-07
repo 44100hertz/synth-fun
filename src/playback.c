c8f10c19bbc983e7299ce5dbfbbbd549000ac8c9
Sam Pagenkopf <ssaammp@gmail.com>
2016-01-07 16:19:50 -0600

    made playback.c use song struct

--- src/playback.c
+++ src/playback.c
@@ -4,9 +4,15 @@
 #include "waves.c"
 #include "quantize.c"
 #include "note.c"
-#include "testSong.c"
 
 int song(int sampleRate, songData song) {
+struct songData testSong;
+
+testSong.BPM = 120;
+testSong.tickRate = 48;
+testSong.key = {c, d, e, f, g, a, b};
+
+	
 	double phase = 0;
 	int tick = -1;
 	int offset = 0;
@@ -14,7 +20,7 @@ int song(int sampleRate, songData song) {
 	
 	// timer cycle
 	for(long timer = 0; song.pattern != -1; timer++) {
-		if(tick < (tick=timer / sampleRate / 60 * BPM * tickRate)) {
+		if(tick < (tick=timer / sampleRate / 60 * song.BPM * song.tickRate)) {
 			slope[i] = ( noteCalcChar(song.key[i + offset]) / sampleRate );
 			++offset;
 		}
