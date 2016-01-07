6e5504a4fe7e776278c6365077005974f02c13b6
Sam Pagenkopf <ssaammp@gmail.com>
2016-01-07 00:21:11 -0600

    added nicer, extensible playback to replace fmtest

--- src/playback.c
+++ src/playback.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 #include <math.h>
-#include "instrument.c"
-#include "fmwaves.c"
+#include "song.c"
+#include "waves.c"
 #include "quantize.c"
 #include "note.c"
-#include "testsong.c"
+#include "testSong.c"
 
-int song(int sampleRate, song song) {
-
+int song(int sampleRate, songData song) {
 	double phase = 0;
-	for(int timer = 0; timer < SAMPLE_RATE * 120; 
+	int tick = -1;
+	int offset = 0;
+	short byteOut;
+	
+	// timer cycle
+	for(long timer = 0; song.pattern != -1; timer++) {
+		if(tick < (tick=timer / sampleRate / 60 * BPM * tickRate)) {
+			slope[i] = ( noteCalcChar(song.key[i + offset]) / sampleRate );
+			++offset;
+		}
+		phase = fmod((timer * slope), 1.0) * 2 - 1;
+
+		byteOut = quantize( sineWave(phase) );
+		
+		putchar(byteOut << 8);
+		putchar(byteOut >> 8);
+	}
 }
+
