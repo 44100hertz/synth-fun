b7aaf23e29879c94a8d6da43970ee2da7b80dd21
Sam Pagenkopf <ssaammp@gmail.com>
2016-01-12 00:58:16 -0600

    fixes to timing in playback.c

--- src/playback.c
+++ src/playback.c
@@ -9,6 +9,8 @@ int playback(int sampleRate, songData song, FILE *path)
 {
 	int offset = 0;
 	double phase = 0;
+	double slope;
+	double env = 0;
 
     enum { STATE_END, STATE_BEAT, STATE_TICK, STATE_GEN };
 	int state = STATE_BEAT;
@@ -18,16 +20,19 @@ int playback(int sampleRate, songData song, FILE *path)
 	while(state != STATE_END) {
 		int nextTick = timer * song.BPM * song.tickRate / sampleRate / 60;
 
-		if(nextTick % song.BPM > nextTick % song.BPM)
+		if((int)(nextTick / song.tickRate) > (int)(tick / song.tickRate))
 			state = STATE_BEAT;
 		else if(nextTick > tick)
 			state = STATE_TICK;
 
 		switch (state) {
 		case STATE_BEAT:
-		case STATE_TICK:
 			++offset;
-			double slope = noteCalcChar(song.key[offset % 7]) / sampleRate;
+			env = 0;
+			double envslope = 1.0 / (double)song.tickRate;
+		case STATE_TICK:
+			slope = noteCalcChar(song.key[offset % 7]) / (double)sampleRate;
+			env += envslope;
 			tick = nextTick;
 		case STATE_GEN:
 			phase += slope;
@@ -36,10 +41,10 @@ int playback(int sampleRate, songData song, FILE *path)
 			++timer;
 		}
 
-		if(timer > sampleRate * 60 / song.BPM) state = STATE_END;
+		if(timer > sampleRate * 60 * 16 / song.BPM) // HARD CODE
+			state = STATE_END;
 		tick = nextTick;
-
-		output_fileS16(phase, path);
+		output_fileS16(gen_sine(phase * gen_1poly(env, 1.0, 1.0)), path);
 	}
 
 	return 0;
