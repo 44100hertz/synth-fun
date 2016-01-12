1fc6fd489e44fdd0d0adc4cd79493574def5a0f2
Sam Pagenkopf <ssaammp@gmail.com>
2016-01-12 00:02:01 -0600

    Fixed output byte handling, it's big endian.
    Removed outated function that was copypaste and no longer needed.

--- src/output.c
+++ src/output.c
@@ -4,7 +4,6 @@
 #include "output.h"
 
 // Turns a wave into a short, clipping out-of-range values
-
 static
 int16_t output_quantize(double wave)
 {
@@ -12,14 +11,8 @@ int16_t output_quantize(double wave)
 		(fmax( fmin( wave, 1.0 ), 0 ) * 0xffff - 0x8000);
 }
 
-void output_printS16(double wave)
-{
-	int16_t input = output_quantize(wave);
-	printf("%c%c", input, input >> 8);
-}
-
 void output_fileS16(double wave, FILE *path)
 {
 	int16_t input = output_quantize(wave);
-	fprintf(path, "%c%c", input, input >> 8);
+	fprintf(path, "%c%c", input >> 8, input);
 }
--- src/output.h
+++ src/output.h
@@ -1,2 +1,1 @@
-void output_printS16(double wave);
 void output_fileS16(double wave, FILE *path);
