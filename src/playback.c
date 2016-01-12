95626f8bf0a7306a3e87834cb77dfeba1cdb4017
Sam Pagenkopf <ssaammp@gmail.com>
2016-01-11 21:04:15 -0600

    Made playback.c into a switch case/while

--- src/playback.c
+++ src/playback.c
@@ -7,24 +7,39 @@
 
 int playback(int sampleRate, songData song)
 {
-	int tick = 0;
 	int offset = 0;
 	double phase = 0;
-	double slope = noteCalcChar(song.key[offset % 7]) / sampleRate;
-	
-	for(int timer = 0; timer < sampleRate * 20; timer++) {		
+	double slope;
+
+    enum { STATE_END, STATE_BEAT, STATE_TICK, STATE_GEN };
+	int state = STATE_BEAT;
+
+	int tick = 0;
+	int timer = 0;
+	while(state != STATE_END) {
 		int nextTick = timer * song.BPM * song.tickRate / sampleRate / 60;
-		if(nextTick > tick) {
+
+		if(nextTick % song.BPM > nextTick % song.BPM)
+			state = STATE_BEAT;
+		else if(nextTick > tick)
+			state = STATE_TICK;
+
+		switch (state) {
+		case STATE_BEAT:
+		case STATE_TICK:
 			++offset;
 			slope = noteCalcChar(song.key[offset % 7]) / sampleRate;
 			tick = nextTick;
+		case STATE_GEN:
+			phase += slope;
+			phase = fmod(phase, 1.0);
+			output_printS16(gen_sine(phase));
+			state = STATE_GEN;
+			++timer;
 		}
 
-		phase += slope;
-		phase = fmod(phase, 1.0);
-
-		// outputs a sine wave for testing
-		output_printS16(gen_sine(phase));
+		if(timer > sampleRate * 60 / song.BPM) state = STATE_END;
+		tick = nextTick;
 	}
 
 	return 0;
