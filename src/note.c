e8d64244465890dd39a8c13c9725787226177de4
Sam Pagenkopf <ssaammp@gmail.com>
2016-01-13 08:43:08 -0600

    note.c bugfix: e# now = F, b# now = C, f# exists

--- src/note.c
+++ src/note.c
@@ -12,16 +12,16 @@ static char parseScale(char noteChar)
 {
 	switch(noteChar) {
 	case 'a': return 57;
-	case 'A': return 58;
-	case 'b':
+	case 'A':
+	case 'b': return 58;
 	case 'B': return 59;
 	case 'c': return 60;
 	case 'C': return 61;
 	case 'd': return 62;
 	case 'D': return 63;
 	case 'e': return 64;
-	case 'E': return 65;
-	case 'f':
+	case 'E':
+	case 'f': return 65;
 	case 'F': return 66;
 	case 'g': return 67;
 	default: return 0;
